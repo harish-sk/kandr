99fea559325a3c1e019d2f7c528d72dc7e67965d
Harish SK <hsk@outlook.com>
2015-09-05 06:54:03 +0530

    Removed Tab

--- e_1_12_print_word_per_line.c
+++ e_1_12_print_word_per_line.c
@@ -2,19 +2,19 @@
 
 int main()
 {
-	int c;
-	int flag = 0;
+   int c;
+   int flag = 0;
 
-	while((c = getchar()) != EOF) {
-		if((c == ' ') || (c == '\t')) {
-			flag = 1;
-		}
-		if((flag == 1) && (c != ' ') && (c != '\t')) {
-			printf("\n%c", c);
-			flag = 0;
-		}else {
-			printf("%c", c);
-		}
-	}
-	return 0;
+   while((c = getchar()) != EOF) {
+      if((c == ' ') || (c == '\t')) {
+         flag = 1;
+      }
+      if((flag == 1) && (c != ' ') && (c != '\t')) {
+         printf("\n%c", c);
+         flag = 0;
+      }else {
+         printf("%c", c);
+      }
+   }
+   return 0;
 }
