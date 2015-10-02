8b9aa946b35d7da79dcdb202130a9f7f508b299f
Harish SK <hsk@outlook.com>
2015-10-02 11:46:14 +0530

    Final version

--- e_1_13_histogram_1.c
+++ e_1_13_histogram_1.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+/* intenal states to count words */
 #define IN 1
 #define OUT 0
 
@@ -7,27 +8,34 @@
 
 int main()
 {
-	int count[MAX_WORD_LEN] = {0};
-	int c, steps = 0;
-	int state = OUT, len = 0, max_count = 1, i = 0, j = 0;
-	int max_column_width = 1;
+  int count[MAX_WORD_LEN] = {0};
+  int c, len = 0;
+  int state = OUT;
+  int i = 0, j = 0;
 
-	while((c = getchar()) != EOF) {
-		if((c == ' ') || (c == '\t') || (c == '\n')) {
-			if((state == IN) &&
-				(len >= 1) && (len < MAX_WORD_LEN)) {
-				count[len]++;
-				max_count = ((count[len] > max_count)?count[len]:max_count);
-			}
-			len = 0;
-         state = OUT;
-      }else if(state == OUT) {
-         state = IN;
-			len++;
-      }else {
-			len++;
+  while((c = getchar()) != EOF) {
+    if(c == ' ' || c == '\t' || c == '\n') {
+      if(state == IN && len >= 1 &&
+	 len < MAX_WORD_LEN) {
+	count[len]++;
       }
-	}
+      len = 0;
+      state = OUT;
+    }else if(state == OUT) {
+      state = IN;
+      len++;
+    }else {
+      len++;
+    }
+  }
+
+  printf("\n");
+  for(i = 1; i < MAX_WORD_LEN; i++) {
+    printf("%2d | ", i);
+    for(j = 0; j < count[i]; j++)
+      printf("*");
+    printf("\n");
+  }
 	
-	return 0;
+  return 0;
 }
